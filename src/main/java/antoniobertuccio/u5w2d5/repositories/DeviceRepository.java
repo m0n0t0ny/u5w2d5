9bc77b91ce836224c82460cdd217d2a47ad7c94e
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-16 11:31:52 +0100

    Add Device and Employee repositories to the project.

--- src/main/java/antoniobertuccio/u5w2d5/repositories/DeviceRepository.java
+++ src/main/java/antoniobertuccio/u5w2d5/repositories/DeviceRepository.java
@@ -1,4 +1,12 @@
 package antoniobertuccio.u5w2d5.repositories;
 
-public class DeviceRepository {
+
+import antoniobertuccio.u5w2d5.entities.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
 }
--- src/main/java/antoniobertuccio/u5w2d5/repositories/EmployeeRepository.java
+++ src/main/java/antoniobertuccio/u5w2d5/repositories/EmployeeRepository.java
@@ -1,4 +1,11 @@
 package antoniobertuccio.u5w2d5.repositories;
 
-public class EmployeeRepository {
+import antoniobertuccio.u5w2d5.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
