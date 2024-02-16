1f067409997ed8497e13b06c76c069dd12e49982
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-16 14:04:17 +0100

    Rename DeviceRepository to DevicesRepository and EmployeeRepository to EmployeesRepository, also update the imports and method names accordingly.

--- src/main/java/antoniobertuccio/u5w2d5/repositories/DevicesRepository.java
+++ src/main/java/antoniobertuccio/u5w2d5/repositories/DevicesRepository.java
@@ -2,11 +2,14 @@ package antoniobertuccio.u5w2d5.repositories;
 
 
 import antoniobertuccio.u5w2d5.entities.Device;
+import antoniobertuccio.u5w2d5.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface DeviceRepository extends JpaRepository<Device, UUID> {
+public interface DevicesRepository extends JpaRepository<Device, UUID> {
+  List<Device> findByEmployee(Employee employee);
 }
--- src/main/java/antoniobertuccio/u5w2d5/repositories/EmployeesRepository.java
+++ src/main/java/antoniobertuccio/u5w2d5/repositories/EmployeesRepository.java
@@ -4,8 +4,11 @@ import antoniobertuccio.u5w2d5.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+public interface EmployeesRepository extends JpaRepository<Employee, UUID> {
+
+  Optional<Employee> findByEmail(String email);
 }
